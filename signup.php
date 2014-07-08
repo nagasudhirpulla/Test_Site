25287b67a828c46941240d4ded3a0c7ef46641a4
nagasudhirpulla <nagasudhirpulla@gmail.com>
2014-07-09 01:14:41 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -26,67 +26,67 @@
       if (!preg_match("/^[a-zA-Z ]*$/",$name)) 
       {
         $filled = false;
-       $nameErr = "Only letters and white space allowed"; 
-     }
-   }
-   else{
-    $filled = false;
-    $nameErr = "Username Required";
-  }
-  if(!empty($_POST['uemail']))
-  {
-    $email = test_input($_POST['uemail']);
-      // check if e-mail address syntax is valid
-    if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) 
-    {
-      $filled = false;
-      $emailErr = "Invalid email format";
-    }
-  }
-  else{
-    $filled = false;
-    $emailErr = "Email Required";
-  }
-  if(!empty($_POST['upass']) || !empty($_POST['ucpass']))
-  {
-    if($_POST['upass'] == $_POST['ucpass'])
-    {
-      if(strlen($_POST['upass'])<=15){
-        $pass = "";
+        $nameErr = "Only letters and white space allowed"; 
       }
-      else{
-        $pass = "";
+    }
+    else{
+      $filled = false;
+      $nameErr = "Username Required";
+    }
+    if(!empty($_POST['uemail']))
+    {
+      $email = test_input($_POST['uemail']);
+      // check if e-mail address syntax is valid
+      if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) 
+      {
         $filled = false;
-        $passErr = "Password length should be below 16 characters";
+        $emailErr = "Invalid email format";
+      }
+    }
+    else{
+      $filled = false;
+      $emailErr = "Email Required";
+    }
+    if(!empty($_POST['upass']) || !empty($_POST['ucpass']))
+    {
+      if($_POST['upass'] == $_POST['ucpass'])
+      {
+        if(strlen($_POST['upass'])<=15){
+          $pass = "";
+        }
+        else{
+          $pass = "";
+          $filled = false;
+          $passErr = "Password length should be below 16 characters";
+        }
+      }
+      else
+      {
+        $filled = false;
+        $passErr = "Passwords mismatch";
+        if(empty($_POST['upass'])||empty($_POST['ucpass']))
+        {
+          $passErr = "Fill both the password fields";
+        }
+        $pass = "";
+
       }
     }
     else
     {
       $filled = false;
-      $passErr = "Passwords mismatch";
-      if(empty($_POST['upass'])||empty($_POST['ucpass']))
-      {
-        $passErr = "Fill both the password fields";
-      }
-     $pass = "";
-     
-   }
- }
- else
- {
-  $filled = false;
-  $passErr = "Password Required";
-}
-}
+      $passErr = "Password Required";
+    }
+  }
 
-function test_input($data) {
- $data = trim($data);
- $data = stripslashes($data);
- $data = htmlspecialchars($data);
- return $data;
-}
-?>
-<h1>Sign up Form<h1>
+  function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+ }
+ ?>
+ <h1>Sign up Form<h1>
   <form method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <table>
       <tr>
@@ -124,24 +124,36 @@ function test_input($data) {
                 <td><span style='font-size:1.2em;'>About You: </span></td>
                 <td><textarea maxlength="500" rows="4" style="width:250px;resize:none;" name="about" ><?php echo "$about"; ?></textarea>
                   <span style='font-size:0.7em;color:red;'>Maximum of 500 characters</span></td> 
-              </tr>
-              <tr>
-                <td colspan="2" align = "center"><button type="submit" value="submit" style="font-size:1em;" >Sign Up</button></td>
-              </tr>
-            </table>
-          </form>
-          <?php
-          if($_SERVER["REQUEST_METHOD"] == "POST" && $GLOBALS['filled'] == true)
-          {
-            echo "<h3>Account created on ".date("d/m/Y")."</h3>";
-            echo "<h3>Your Username is ".$name."</h3>";
-            echo "<h3>Your Email Id is ".$email."</h3>";
-            $about = str_replace("\n", "<br>", $about);
-            echo "<h3> About You :<br> $about</h3>";
+                </tr>
+                <tr>
+                  <td colspan="2" align = "center"><button type="submit" value="submit" style="font-size:1em;" >Sign Up</button></td>
+                </tr>
+              </table>
+            </form>
+            <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST" && $GLOBALS['filled'] == true)
+            {
+              $date = "gfkh";
+              $table = "customers";
+              echo "<h3>Account created on ".$date."</h3>";
+              echo "<h3>Your Username is ".$name."</h3>";
+              echo "<h3>Your Email Id is ".$email."</h3>";
+              $about = str_replace("\n", "<br>", $about);
+              echo "<h3> About You :<br> $about</h3>";
             //create database connection to database Bullshit and upload into the table Customers with columns id, name, password, email, date, about
-            
-          }
+              $con=mysqli_connect("localhost","root","password123","bullshit");
+            // Check connection
+              if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+              }
 
-          ?>
-        </body>
-        </html>
+              $query = "INSERT INTO $table "."(`name`, `password`, `email`, `date1`, `about`) "."VALUES "."($name, $pass, $email, $date, $about)";
+
+              mysqli_query($con,$query);
+
+              mysqli_close($con);
+            }
+
+            ?>
+          </body>
+          </html>
